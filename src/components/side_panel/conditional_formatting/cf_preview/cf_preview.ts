38253d8267cdf43ae809812039618044e1c65c13
rmbh-odoo <rmbh@odoo.com>
2025-09-16 18:51:07 +0530

    [REF] side_panel: split CF into main and editor panels, use store
    Before this commit:
    - The cf side panel handled both list and editor in a single panel.
    - The logic of the main panel and the editor was mixed,
      preventing direct access to the editor.
    - Business and UI logic for the editor were coupled.
    
    After this commit:
    - Use a store to manage the state of the editor panel.
    - Make the main panel and editor panel independent.
    - Change undo behavior in the editor to redirect back to the main panel.
    - Refactor related test cases.
    
    Task: 4982596

--- src/components/side_panel/conditional_formatting/cf_preview/cf_preview.ts
+++ src/components/side_panel/conditional_formatting/cf_preview/cf_preview.ts
@@ -10,16 +10,18 @@ import { CfTerms } from "../../../translations_terms";
 
 interface Props {
   conditionalFormat: ConditionalFormat;
-  onPreviewClick: () => void;
   onMouseDown: (ev: MouseEvent) => void;
   class: string;
 }
 
-export class ConditionalFormatPreview extends Component<Props, SpreadsheetChildEnv> {
-  static template = "o-spreadsheet-ConditionalFormatPreview";
-
+export class ConditionalFormattingPreview extends Component<Props, SpreadsheetChildEnv> {
+  static template = "o-spreadsheet-ConditionalFormattingPreview";
+  static props = {
+    conditionalFormat: Object,
+    onMouseDown: Function,
+    class: String,
+  };
   icons = ICONS;
-
   private ref = useRef("cfPreview");
 
   setup() {
@@ -61,6 +63,13 @@ export class ConditionalFormatPreview extends Component<Props, SpreadsheetChildE
     }
   }
 
+  editConditionalFormat() {
+    this.env.replaceSidePanel("ConditionalFormattingEditor", "ConditionalFormatting", {
+      cf: this.props.conditionalFormat,
+      isNewCf: false,
+    });
+  }
+
   deleteConditionalFormat() {
     this.env.model.dispatch("REMOVE_CONDITIONAL_FORMAT", {
       id: this.props.conditionalFormat.id,
@@ -81,10 +90,3 @@ export class ConditionalFormatPreview extends Component<Props, SpreadsheetChildE
     }));
   }
 }
-
-ConditionalFormatPreview.props = {
-  conditionalFormat: Object,
-  onPreviewClick: Function,
-  onMouseDown: Function,
-  class: String,
-};
